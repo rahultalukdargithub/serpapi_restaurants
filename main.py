132c4f9d37859c735367fb4ac0ad6c991f29aed2
Rahul Talukdar <rahultalukdar33714@gmail.com>
2025-08-06 13:29:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def welcome():
 @app.get("/scrape/location/")
 def get_restaurants_by_location(city: str = Query(..., example="Kolkata"),area: Optional[str] = Query(None, example="park street"), limit: int = 10):
     # create_excel_if_not_exists(EXCEL_FILE)
+    print("yes1")
     data = scrape_by_location(city,area, limit)
     # save_to_excel(EXCEL_FILE, data)
     return {"message": f"{len(data)} results saved for {city}", "data": data}
@@ -26,5 +27,6 @@ def get_restaurant_by_name(name: str):
     data = scrape_by_name(name)
     # save_to_excel(EXCEL_FILE, data)
     return {"message": f"Details saved for: {name}", "data": data}
+
 
 
