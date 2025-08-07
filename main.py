13b08fe09fa8095bb8235b44ddcb363ba9c42d04
Rahul Talukdar <rahultalukdar33714@gmail.com>
2025-08-08 00:05:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI, Query
-from scraper import scrape_by_location, scrape_by_name, save_to_excel, create_excel_if_not_exists
+from scraper import scrape_by_location, scrape_by_name
 from typing import Optional
 
 app = FastAPI()
-EXCEL_FILE = "restaurants.xlsx"
-# create_excel_if_not_exists(EXCEL_FILE)
+
 
 @app.get("/")
 def welcome():
@@ -12,21 +11,20 @@ def welcome():
 
 
 @app.get("/scrape/location/")
-def get_restaurants_by_location(city: str = Query(..., example="Kolkata"),area: Optional[str] = Query(None, example="park street"), limit: int = 10):
-    # create_excel_if_not_exists(EXCEL_FILE)
-    print("yes1")
-    data = scrape_by_location(city,area, limit)
-    # save_to_excel(EXCEL_FILE, data)
-    return {"message": f"{len(data)} results saved for {city}", "data": data}
+def get_restaurants_by_location(
+    city: str = Query(..., example="Kolkata"),
+    area: Optional[str] = Query(None, example="park street"),
+    limit: int = 10
+):
+    data = scrape_by_location(city, area, limit)
+    return {
+        "message": f"{len(data)} results saved for {city}",
+        "data": data
+    }
 
 
 
 @app.get("/scrape/name/")
 def get_restaurant_by_name(name: str):
-    # create_excel_if_not_exists(EXCEL_FILE)
     data = scrape_by_name(name)
-    # save_to_excel(EXCEL_FILE, data)
     return {"message": f"Details saved for: {name}", "data": data}
-
-
-
